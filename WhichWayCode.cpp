8ba89ff6da5ec98367b07b22569bec2cb9edea1e
Milk-Dude <numbb3rs@outlook.com>
2016-05-14 14:28:24 +1200

    Olly's code + some movement mechanics
    Olly's detection logic + some added movement logic.

--- WhichWayCode.cpp
+++ WhichWayCode.cpp
@@ -38,8 +38,8 @@ int main(){
 		int sum = 0; //Will determine whether the line is on the left or right side of the image (or even there at all)
 		for(x=0, x<320, x++){ //Camera is 320 long x 240 high, this will scan the centre line
 			int r = get_pixel(120, x, 1); //Scan pixel for red value
-			int g = get_pixel(120, x, 2); //Scan pixel for green value
-			int b = get_pixel(120, x, 3); //Scan pixel for blue value
+			int g = get_pixel(120, x, 2); //green pixel
+			int b = get_pixel(120, x, 3); //blue pixel
 			if(r > 150 && g > 150 && b > 150){ //If a white pixel is detected (for now assuming white = anything over 150)
 				pixelColour = 1;
 			}
@@ -49,13 +49,19 @@ int main(){
 			int value = (x - 160) * pixelColour; //If white pixel on left, will be negative, if on right, will be positive
 			sum += value; //Negative = line on left of centre, positive = line on right of centre (magnitude of number represents the distance away from centre)		
 		}
+		motor_v = 0; // if between 200 &-200 then the speed will default to 127
 		if(sum > 200){ //If line is farily to the left (will have to test line size and tinker with '200' value)
-			//Turn to the right
+			//motor_v++; // increases speed of the left wheel and lowers the speed of the right
+			motor_v = motor_v + .4*(sum);
 		}
 		else if(sum < -200){ //If line is on the left
-			//Turn to the left
-		}
-		Sleep(100000000, 0); //SLeep for 0.1 of a second
+			//motor_v--;// increases right wheel and decreases left
+			motor_v = motor_v + .4*(sum); // is =ve bc of the -ve sum
+		} // Current code may cause the pi to zig zag around the line aslong as there's white on the camera
+		// Will integrate PID soon.
+        set_motor(1, 127 + motor_v);
+        set_motor(2, 127 - motor_v);
+        Sleep(100000000, 0); //SLeep for 0.1 of a second
 	}
 
 return 0;}
